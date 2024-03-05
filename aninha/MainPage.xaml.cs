2df2352f24130f210f56118c81646aa41683f5d6
anna <annaheloisa1809@gmail.com>
2024-03-05 09:09:26 -0300

    coloquei o botão

--- aninha/MainPage.xaml.cs
+++ aninha/MainPage.xaml.cs
@@ -1,13 +1,15 @@
-﻿namespace aninha;
+﻿using System.Security.Cryptography;
+
+namespace aninha;
 
 public partial class MainPage : ContentPage
 {
 
 	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-
+	void Button_Clicked(System.Object sender, FileSystemInfo.EventArgs e)
+    { 
+	int indice = random.Next(0, 9);
+    }
+   
 }
 
