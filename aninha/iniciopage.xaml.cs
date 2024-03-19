f7bf0470e8ddc663885f129e037630f0e548a469
aninha <annaheloisa1809@gmail.com>
2024-03-19 08:55:38 -0300

    p/continou

--- aninha/iniciopage.xaml.cs
+++ aninha/iniciopage.xaml.cs
@@ -10,6 +10,18 @@ public partial class iniciopage : ContentPage
 		InitializeComponent();	
 	}
 	
-     
+    void Button_Clicked(object sender, EventArgs e)
+	{
+       Application.Current.iniciopage.page = new meiopage();
+    }
+	void ButtonSobre_Clicked(object sender, EventArgs e)
+    { 
+       COMEÇARFrame.IsVisible = true;
+    }
+	void ButtonContinuar_Clicked(object sender, EventArgs e)
+    { 
+       ContinuarFrame.IsVisible = true;
+	}
+	
 }
    
